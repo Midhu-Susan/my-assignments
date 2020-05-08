479c8c8a3fc4e33438e6e330b81dded8ab7616df
Midhu-Susan <64888715+Midhu-Susan@users.noreply.github.com>
2020-05-08 11:15:13 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+# fibonacci series 0,1,2,3
+# n = 1,2,3.... n
+n = int(input("Enter n : "))
+a=0
+b=1
+c=a+b
+print(a)
+print(b)
+while c<=n:
+  print(c)
+  a=b
+  b=c
+  c=a+b
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
